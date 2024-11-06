68cc90bf27841e530c49d017aab700a11976b5d6
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-06 10:01:41 +0530

    Special Template Features & Syntax

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,82 @@
 from django.shortcuts import render
-
+from datetime import date
 # Create your views here.
+all_posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountain.jpg",
+        "author": "Saurabh",
+        "date": date(2024, 7, 21),
+        "title": "Mountain Hiking",
+        "description": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "Mountain.jpg",
+        "author": "Saurabh",
+        "date": date(2024, 3, 10),
+        "title": "Programming Is Great!",
+        "description": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "Saurabh.jpg",
+        "author": "Saurabh",
+        "date": date(2024, 8, 5),
+        "title": "Nature At Its Best",
+        "description": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
+def get_date(post):
+    return post['date']
 
 def starting_page(request):
-    return render(request , "blog/index.html")
+    sorted_posts = sorted( all_posts ,key=get_date)
+    latest_posts  = sorted_posts[-3:]
+
+    # print(sorted_posts)
+    return render(request , "blog/index.html",{
+        "posts": latest_posts
+    })
 
 
 def posts(request):
